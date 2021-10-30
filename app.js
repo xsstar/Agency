c41d07c9070b3abb7ac304a10602a982b08b8a6f
havva polatlı <74872560+xsstar@users.noreply.github.com>
2021-10-30 13:43:38 +0300

    Upload Images

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const fileUpload = require('express-fileupload');
 const path = require('path');
 const ejs = require('ejs');
 const Photo = require('./models/Photo');
+const fs = require('fs');
 
 const app = express();
 
@@ -15,55 +17,63 @@ mongoose.connect('mongodb://localhost/agency-db', {
 app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
-app.use(express.urlencoded({extended:true}))
-app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(fileUpload());
 
-app.get('/', async(req, res) => {
-  const photos = await Photo.find({})
+app.get('/', async (req, res) => {
+  const photos = await Photo.find({}).sort('-dateCreated');;
   res.render('index', {
-    photos
+    photos,
   });
 });
 
 app.get('/photos/:id', async (req, res) => {
   //console.log(req.params.id)
-  const photo = await Photo.findById(req.params.id)
+  const photo = await Photo.findById(req.params.id);
   res.render('photo', {
-    photo
-  })
+    photo,
+  });
 });
 
- 
 app.get('/add', (req, res) => {
   res.render('add');
-  
 });
 
 app.get('/services', (req, res) => {
   res.render('services');
-  
 });
 
 app.get('/team', (req, res) => {
   res.render('team');
-  
 });
 
 app.get('/about', (req, res) => {
   res.render('about');
-  
 });
 
 app.get('/about', (req, res) => {
   res.render('about');
-  
 });
 
-app.post('/photos', async (req, res) => { 
-  await Photo.create(req.body)            
-  res.redirect('/')
-});
+app.post('/photos', async (req, res) => {
+  const uploadDir = 'public/uploads';
 
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir);
+  }
+
+  let uploadeImage = req.files.image;
+  let uploadPath = __dirname + '/public/uploads/' + uploadeImage.name;
+
+  uploadeImage.mv(uploadPath, async () => {
+    await Photo.create({
+      ...req.body,
+      image: '/uploads/' + uploadeImage.name,
+    });
+    res.redirect('/');
+  });
+});
 
 const port = 3000;
 app.listen(port, () => {
